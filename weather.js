f80a5db6c486eeed9f5b06b27b5a8a3d1ed9f005
Franco Talarico <francotalarico93@gmail.com>
2016-03-12 18:27:32 -0300

    HTML Structure developed

---
+++ weather.js
@@ -0,0 +1,26 @@
+/**
+ * Created by franco on 12/03/16.
+ */
+$(document).ready(function(){
+    var
+        lat,
+        lon,
+        apiQuery,
+        apiKey = "efd065b7771a39dd258cc02b2ba90e2f",
+        currentData = [];
+
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function(position){
+            lat = position.coords.latitude;
+            lon = position.coords.longitude;
+            apiQuery = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&APPID="+apiKey;
+
+            $.getJSON(apiQuery, function(data){
+                console.log(data);
+                return currentData = data;
+            })
+        });
+    } else {
+        /* geolocaiton IS NOT available */
+    }
+});
